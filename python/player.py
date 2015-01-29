c01f81897133d8aefada43072bf0dc436fca5057
Marvin T <mthielk@ucsd.edu>
2015-01-28 23:01:11 -0800

    distributed color score player

--- python/player.py
+++ python/player.py
@@ -1,6 +1,8 @@
 from coordinates import Coordinate
 import random
 import sys
+import numpy as np
+from numpy.random import randn
 
 
 if sys.version_info >= (3,):
@@ -115,6 +117,33 @@ class IllegalPlayer(Player):
                    
     def turn(self):
         return random.choice(self.coords)
+
+class DistributedColorScorePlayer(Player):
+    def __init__(self):
+        Player.__init__(self)
+        self.coords = [#Coordinate(-1,-1),
+                       #Coordinate( 0,-1),
+                       Coordinate( 1, 0),
+                       #Coordinate( 1,-1),
+                       Coordinate(-1, 0),
+                       #Coordinate( 0, 0),
+                       Coordinate(-1, 1),
+                       Coordinate( 0, 1),
+                       Coordinate( 1, 1)
+                       ]
+        dims_to_use = 100
+        self.reducer = randn(16, dims_to_use)
+        self.reducer -= np.tile(np.mean(self.reducer, axis=1).reshape((16,1)), (1, dims_to_use))
+
+    def turn(self):
+        genome_bits = np.array([self.bit_at(i) for i in range(100)])
+        color_scores = self.reducer.dot(genome_bits)
+        max_score = max([color_scores[self.vision_at(c.x, c.y)] for c in self.coords if self.vision_at(c.x, c.y)>=0])
+        restricted_coords = [c for c in self.coords if self.vision_at(c.x, c.y)>=0 and color_scores[self.vision_at(c.x,c.y)] == max_score]
+
+        return random.choice(restricted_coords)
+
+
         
 
-PLAYER_TYPE = ColorScorePlayer
+PLAYER_TYPE = DistributedColorScorePlayer
